5dc7aa527c514b3acf938f466ff8f8bf989628ed
lapshaffer <lapshaffer@gmail.com>
2021-07-12 13:41:17 -0400

    Added files and to-do lists for html, css and js components

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// TO-DO:
+// Search bar functionality
+    // When a city is entered, the weather data for that city is pulled and displayed on the screen
+    // Event listener for button
+    // Fetch requests to OpenWeather API
+// Local storage
+    // Cities that have been searched for this session are stored in the sidebar and can be clicked to search for them again
+    // Buttons for recently searched cities
+// Write weather data to designated containers on the screen
+    // Pull specific data: the date (may need Moment), an icon representation of weather conditions(fontawesome), the temperature, the humidity, the wind speed, and the UV index
+    // When pulling today's date, also pull data for the next 5 days
+    // UV Index coloration
