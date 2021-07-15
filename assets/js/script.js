720a8a280e083bcffa5a8628bffc832910e6935d
lapshaffer <lapshaffer@gmail.com>
2021-07-14 22:05:20 -0400

    Got API pulls working, half-way through debugging cities array

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,15 @@
     // When pulling today's date, also pull data for the next 5 days
     // UV Index coloration
 
+// current
+    // current.dt
+    // current.temp
+    // current.humidity
+    // current.wind_speed
+    // current.uvi
+    // current.weather.id
+        // https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
+
 var searchBar = document.querySelector('#input-group');
 var citySearch = document.querySelector('#form1')
 var cityDate = document.querySelector('#cityDate');
@@ -21,8 +30,19 @@ var apiKey = "9c1c9bc50fd04a36296ec59a79ae6f60";
     // Cities that have been searched for this session are stored in the sidebar and can be clicked to search for them again
     // Buttons for recently searched cities
 function writeRecentSearches() {
-
+    var lastCity = localStorage.getItem("lastCity");
+    recentSearches.innerHTML = lastCity;
 };
+
+var recentCities = [];
+
+saveBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    var city = document.querySelector("#form1").value;
+    recentCities.push(city);
+    localStorage.setItem("lastCity", recentCities);
+    writeRecentSearches();
+});
 
 // Search bar functionality
     // When a city is entered, the weather data for that city is pulled and displayed on the screen
@@ -30,16 +50,40 @@ function writeRecentSearches() {
     // Fetch requests to OpenWeather API
 function handleSearchFormSubmit(event) {
     event.preventDefault();
-  
     citySearch = document.querySelector('#form1').value;
 
-    queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=" + apiKey;
-  searchApi(citySearch, apiKey);
+    queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + apiKey; 
 
+    fetch(queryUrl).then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        getUVI(data.coord.lat, data.coord.lon);
+        getForecast(citySearch);
+    });
 };
 
-searchBar.addEventListener('submit', handleSearchFormSubmit);
-
-function searchApi(query, format) {
-    var queryUrl = 'https://api.openweathermap.org/data/2.5/forecast';
+function getUVI(lat, lon) {
+    console.log(lat, lon);
+    uviURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+    fetch(uviURL).then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
 };
+
+function getForecast(city) {
+    console.log(city);
+    forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=" + apiKey;
+    fetch(forecastURL).then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+};
+
+saveBtn.addEventListener('click', handleSearchFormSubmit);
